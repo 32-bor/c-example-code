710c9dd333659d9a7bad078296bdde202c476b49
Bawa <7207459+bawasv@users.noreply.github.com>
2024-06-09 16:41:04 -0400

    Finding min in array

--- find_min.c
+++ find_min.c
@@ -5,13 +5,13 @@ int find_min(int[],int);
 
 int main()
 {
-	int myArray1[]={9,435,43,654,45,76,9,3,18};
+	int myArray1[]={9,5,4,12,45,7,9,3,18};
 	int myArray2[]={4,9,8,6,2};
 	int min1=find_min(myArray1,9);
-	int min2=find_min(myArray2,5);
+	// int min2=find_min(myArray2,5);
 
-	printf("Min in array 1:%d\n",min1);
-	printf("Min in array 2:%d",min2);
+	printf("\nMin in array :%d\n",min1);
+	// printf("Min in array 2:%d",min2);
 
 	printf("\n");
 	return 0;
@@ -23,8 +23,12 @@ int find_min(int array[],int size)
 
 	for(int i=1;i<size;i++)
 	{
+		printf("Checking myArray[%d] = %d:\n", i,array[i]);
 		if(array[i]<min)
+		{
 			min=array[i];
+			printf("New min found: %d\n",array[i]);
+		}
 	}
 	return min;
 }
