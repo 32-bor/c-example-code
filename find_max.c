1dcaec0a0cfb0b58bd23d10ba9f557a9e687e517
Bawa <7207459+bawasv@users.noreply.github.com>
2024-06-09 16:46:58 -0400

    Finding max in array

--- find_max.c
+++ find_max.c
@@ -6,12 +6,12 @@ int find_max(int [],int);
 int main()
 {
 	int myArray1[]={9,435,43,654,45,76,9,3,18};
-	int myArray2[]={4,9,8,6,2};
+	// int myArray2[]={4,9,8,6,2};
 	int max1=find_max(myArray1,9);
-	int max2=find_max(myArray2,5);
+	// int max2=find_max(myArray2,5);
 
-	printf("Min in array 1: %d\n",max1);
-	printf("Min in array 2: %d",max2);
+	printf("\nMin in array : %d\n",max1);
+	// printf("Min in array 2: %d",max2);
 
 	printf("\n");
 	return 0;
@@ -23,8 +23,12 @@ int find_max(int array[],int size)
 
 	for(int i=1;i<size;i++)
 	{
+		printf("Checking myArray[%d] = %d\n",i,array[i]);
 		if(array[i]>max)
+		{
 			max=array[i];
+			printf("new max found: %d\n", array[i]);
+		}
 	}
 	return max;
 }
